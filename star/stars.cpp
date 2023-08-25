283c9f94dc74fb12331d4838bd7e93819813e326
LRG1998 <ericwarfish212@gmail.com>
2023-08-25 12:45:55 -0500

    Made makefiles

--- star/stars.cpp
+++ star/stars.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    unsigned int row = 1;
-    for(unsigned int row = 1; row < 10; row++){
+    int row = 1;
+    for(int row = 1; row < 10; row++){
         if(row % 2 < 1){
             cout<<" * * * * * \n";
         }
